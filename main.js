82a86db02baf1627cd50ed0e4d749ff98979d1d4
Ayush Gupta <82319063+skilloo-coder@users.noreply.github.com>
2022-01-15 12:21:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ var slack = document.getElementById('preloader');
  function myfun(){
    slack.style.display = 'none';
  }
+ 
+ var link = document.getElementById('floating');
+  function ayush(){
+    link.style.display = 'flex';
+    link.style.alignItems = 'center';
+    link.style.justifyContent = 'center';
+  }
